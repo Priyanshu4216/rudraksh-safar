163662fae7a7558a881612317320c146992db944
Priyanshu Chaudhary <Priyanshuc2003@gmail.com>
2026-05-20 01:29:13 +0530

    chore: clean up unnecessary files and add missing routes to sitemap

--- travel-compass-hub-main/compute_md5.ps1
+++
@@ -1,4 +0,0 @@
-param([string]$inputString)
-$md5 = [System.Security.Cryptography.MD5]::Create()
-$hash = [BitConverter]::ToString($md5.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($inputString)))
-Write-Output $hash.Replace("-", "").ToLower()
